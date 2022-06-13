b11a973f0ffc240a3aa55d1a00bb2c15aa664ef9
JoelGrayson <joel@joelgrayson.com>
2022-06-13 14:31:15 -0400

    Spinning logo

--- components/Logo.js
+++ components/Logo.js
@@ -3,10 +3,13 @@ import styles from '../styles/logo.module.css';
 import Link from 'next/link';
 
 export default function Logo() {
-    // const [rotating, setRotating] = useState(false);
+    const [rotating, setRotating] = useState(true);
 
     return (<Link href='/'><a>
-        <div className={styles.container}>
+        <div className={styles.container}
+            onMouseEnter={_=>setRotating(false)}
+            onMouseLeave={_=>setRotating(true)}
+        >
             <img
                 className={styles.entire}
                 src="/images/SEB Logo.png" alt="Logo" height='80px' width='80px'
@@ -14,9 +17,9 @@ export default function Logo() {
             <img alt="Outer logo" height='80px' width='80px'
                 className={styles.inner}
                 src="/images/rotatable/inner.png"
-                />
+            />
             <img alt="Inner logo" height='80px' width='80px'
-                className={styles.outer}
+                className={rotating ? styles.rotatingOuter : styles.outer}
                 src="/images/rotatable/outer.png"
             />
         </div>
