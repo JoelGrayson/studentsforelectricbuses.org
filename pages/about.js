da6e3fdd5e00c2ce69f6be91642b4a183eeec330
JoelGrayson <joel@joelgrayson.com>
2022-06-19 09:19:06 -0700

    Compressed images

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+export default function About() {
+    return (<>
+        <p>The Hill School Combating Climate Change Committee is a coalition between the three hill schools (Riverdale, Fieldston, and Horace Mann) to combat climate change with interschool solutions. Its first focus has been integrating electric school buses into SuperSelby&apos;s fleet.</p>
+
+        <h2>Members</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Riverdale Country School</th>
+                    <th>Fieldston</th>
+                    <th>Horace Mann</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Joel Grayson</td>
+                    <td>Bri Bernard</td>
+                    <td>Spencer Rosenberg</td>
+                </tr>
+                <tr>
+                    <td>Riley Wittman</td>
+                    <td></td>
+                    <td>Claire Goldberg</td>
+                </tr>
+                <tr>
+                    <td>Talia Mamaysky</td>
+                </tr>
+            </tbody>
+        </table>
+    </>);
+}
