a9aecb9d463bfafa5cdd981ba6f5a437df939b44
JoelGrayson <joel@joelgrayson.com>
2022-06-19 09:28:11 -0700

    Tailwind CSS

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import IdlingEngine from '../components/IdlingEngine';
 export default function Home() {
     return (<>
         <Header/>
-        <h1>Home</h1>
+        <h1 className='text-center'>Students for Electric Buses</h1>
         <IdlingEngine/>
     </>);
 }
