b7d55cdb53025851448f73187af9a1137e6f3d53
Joel Grayson <joel@joelgrayson.com>
2023-04-29 11:02:57 -0400

    state-of-the-e-bus and dont-forget-private-schools

--- pages/the-state-of-the-e-bus.tsx
+++ pages/the-state-of-the-e-bus.tsx
@@ -1,5 +1,4 @@
 import Page from '../components/PageContainer';
-import IdlingEngine from '../components/IdlingEngine';
 import styles from '../styles/home/home.module.css';
 import Link from 'next/link';
 
@@ -7,7 +6,8 @@ export default function StateOfTheEBus() {
     return <Page>
         <main className={styles.main}>
             <h1 className='text-center pb-8'>The State of the E-Bus</h1>
-
+            <h3>In New York State</h3>
+            <p>All new purchased school buses <b>must be electric</b> in 2027. Although the state is mandating <b>both public and private schools</b>, it is only helping public schools acheive this goal. Reference: <Link href='https://www.nyserda.ny.gov/All-Programs/Electric-School-Buses'>nyserda.ny.gov/All-Programs/Electric-School-Buses</Link>.</p>
         </main>
     </Page>;
 }
