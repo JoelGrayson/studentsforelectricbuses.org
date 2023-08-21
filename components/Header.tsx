2a04c46a63b7ab210df1540a4e15b893d6ca2630
Joel Grayson <joel@joelgrayson.com>
2023-08-20 20:46:13 -0400

    header dropdown and buttons light up when selected

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,14 @@ import Logo from './Logo';
 import styles from '../styles/header/header.module.css';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { list as meetingsList } from '@/pages/meetings/list';
+import { useState } from 'react';
 
 export default function Header({title, children}: {title?: String, children?: any}) { //takes in parameters for <Head>
+    const { route }=useRouter();
+    // console.log({route, asPath, pathname});
+    
     return <>
         <Head> {/* Default head options */}
             <title>{title ? `${title} | Students for Electric Buses`: 'Students for Electric Buses'}</title>
@@ -16,14 +22,16 @@ export default function Header({title, children}: {title?: String, children?: an
             <nav>
                 <ul className='m:grid m:grid-cols-3 m:grid-rows-2 m:place-items-center'>
                     {/* w-full m:p-0 m:grid grid-cols-3 grid-rows-2 */}
-                    <Link href="/"><li><button>Home</button></li></Link>
-                    <Link href="/meetings"><li><button>Meetings</button></li></Link>
+                    <Li href="/">Home</Li>
+                    <Li href="/meetings" dropdown={meetingsList.map(e=>({ title: e.title, url: e.hyphenatedTitle }))}>Meetings</Li>
                         {/* TODO: insert v down arrow for fast selecting */}
-                    <Link href="/resolutions"><li><button>Resolutions</button></li></Link>
+                    <Li href="/resolutions">Resolutions</Li>
                         {/* TODO: insert v down arrow for fast selecting */}
-                    <Link href="/record"><li><button>Record</button></li></Link>
-                    <Link href="/join"><li><button>Join</button></li></Link>
-                    <Link href="/contact"><li><button>Contact</button></li></Link>
+                        {/* <button>v</button> */}
+                    
+                    <Li href="/record">Record</Li>
+                    <Li href="/join">Join</Li>
+                    <Li href="/contact">Contact</Li>
                     
                     
                     {/* Old */}
@@ -34,4 +42,52 @@ export default function Header({title, children}: {title?: String, children?: an
             </nav>
         </header>
     </>;
+
+    type titleUrlT={ title: string; url: string };
+    function Li({children, href, dropdown}: {children: any, href: string; dropdown?: titleUrlT[] }) {
+        const [open, setOpen]=useState(false);
+
+        return <div className='relative'>
+            <Link href={href}>
+                <li style={{
+                    marginRight: dropdown ? 0 : undefined,
+                    display: 'inline-block'
+                }}>
+                    <button style={{
+                        backgroundColor: route===href ? '#90ee90' : undefined,
+                        borderColor: route===href ? 'darkgreen' : undefined,
+                        borderTopRightRadius: dropdown ? 0 : undefined,
+                        borderBottomRightRadius: dropdown ? 0 : undefined
+                    }}>
+                        {children}
+                    </button>
+                </li>
+            </Link>
+            { dropdown && <>
+                <button
+                    style={{
+                        display: 'inline-block',
+                        marginRight: '.2rem',
+                        borderTopLeftRadius: 0,
+                        borderBottomLeftRadius: 0,
+                        borderLeft: 'none',
+                    }}
+                    onClick={()=>setOpen(!open)}
+                >
+                    {open ? '^' : 'v'}
+                </button>
+            </> }
+            { open && dropdown && <div className='absolute top-[2.3rem] left-0'>
+                {dropdown.map((item, index)=>{
+                    return <div className='w-full bg-white hover:bg-gray-300 p-3' style={{
+                        border: '1px solid black',
+                        borderTopWidth: index===0 ? 1 : 0,
+                    }} key={item.title}>
+                        <Link href={`${href}/${item.url}`}>{item.title}</Link>
+                    </div>;
+                })}
+            </div> }
+        </div>;
+    }
 }
+
--- pages/record/index.tsx
+++ pages/record/index.tsx
@@ -8,13 +8,19 @@ export default function Record() {
 
         <h3>Historical Timeline</h3>
         <iframe width="560" height="315" className='m:w-fit m:h-fit' src="https://www.youtube.com/embed/dq4KiZ5Mov0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
-        <br />
+        <br /><br />
 
         <h3>Photos</h3>
         <figure>
             <Image src='/images/club-meeting.jpg' alt='5.31.23 Club Meeting' width={500} height={375} />
             <figcaption>5.31.23 Club Meeting</figcaption>
         </figure>
+        <br /><br />
+        
+        <h3>2022–23 Club Advertisement Video</h3>
+        <iframe width="560" height="315" className='m:w-fit m:h-fit' src="https://www.youtube.com/embed/6Hh6h2r4Sbw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <br /><br />
+
         <figure>
             <Image src='/images/sdgs.jpg' alt='Sustainable Development Goals' width={500} height={375} />
             <figcaption>We meet ten of the seventeen UN sustainable development goals</figcaption>
