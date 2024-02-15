75ad86189cfee3d2ca55b3ae6da69d84dada7dc7
Joel Grayson <joel@joelgrayson.com>
2024-02-15 15:13:00 -0500

    pie chart emissions chart

--- pages/the-state-of-e-buses.tsx
+++ pages/the-state-of-e-buses.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Page from '../components/PageContainer';
 import styles from '../styles/home/home.module.css';
 import Link from 'next/link';
@@ -26,6 +27,7 @@ export default function StateOfTheEBus() {
             </ul>
 
             <h3>In New York State</h3>
+            <Image src="/images/NYS Transportation Sector Emissions cropped.png" width={250} height={195.25} alt="NYS Emissions" className='d:float-right' />
             <p>New York State plans 85 percent reduction in greenhouse gas emissions by 2050. This will not be possible without decarbonizing the transportation sector, which accounts for 47% of New York State’s CO2 Emissions <Source>https://cbcny.org/research/4-facts-about-new-yorks-transportation-emissions</Source>. 2.3 million students, approximately 10 percent of our nation&apos;s pupils, are transported to school on New York State&apos;s 45,000 buses <Source>https://www.nyserda.ny.gov/All-Programs/Electric-School-Buses</Source>. All new purchased school buses <b>must be electric</b> in 2027. Although the state is mandating <b>both public and private schools</b>, it is only helping public schools acheive this goal <Source>https://www.nyserda.ny.gov/All-Programs/Electric-School-Buses</Source>.</p>
             <p>The American Lung Association in New York has noted that moving the State&apos;s entire vehicle fleet to clean transportation would result in up to 159,000 avoided asthma attacks while generating $68.2 billion in public health benefits statewide. <Source>https://www.nyserda.ny.gov/All-Programs/Electric-School-Buses</Source>.</p>
             
